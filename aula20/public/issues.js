849312713e294d7bad682fd93f11820b25a4a83f
Luís Falcão <lfalcao@cc.isel.pt>
2020-05-25 22:07:10 +0100

    Lesson 20: Creating a new issue  supported with a form inserted in index.html  (shows all issues) page.

--- aula20/public/issues.js
+++ aula20/public/issues.js
@@ -1,17 +1,39 @@
 window.onload = function (e) {
+  const config = {
+    host: 'localhost',
+    port: 1904,
+    baseApi: "it/api/"
+  }
+
+
+  function UriManager() {
+    const baseUri = `http://${config.host}:${config.port}/${config.baseApi}`
+    this.getAllIssuesUri = () => `${baseUri}issues/`
+    this.getIssueUri = (id) => `${baseUri}issue/${id}`
+    this.getAddIssueUri = () => `${baseUri}issues/`
+  }
+
+  const uriManager = new UriManager()
+
+
+
   let functionName = window.location.pathname.substring(1).split(".")[0]
 
   const router = {
-    index: index,   
+    index: index,
     issue: issue
   }
 
   router[functionName]()
 
   function index() {
-    fetch('/it/api/issues')
+    document.querySelector("#create-issue-btn").onclick = createIssue
+
+    fetch(uriManager.getAllIssuesUri())
       .then(rsp => rsp.json())
       .then(showItems)
+
+
 
     function showItems(items) {
 
@@ -24,12 +46,40 @@ window.onload = function (e) {
 
       const table = document.querySelector("#items")
       table.innerHTML += itemsHtml
+    }
+
+
+    function createIssue() {
+      let issue = {}
+      document.querySelectorAll(".issue-data input").forEach(input => issue[input.id] = input.value)
+
+      const issueBody = JSON.stringify(issue)
+
+      console.log(`Issue obj ${issueBody}`)
+
+      const uri = uriManager.getAddIssueUri();
+      fetch(uri, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+        body: issueBody
+      }).then(processResponse)
+    }
+
+    function processResponse(response) {
+      if(response.ok) {
+        location.reload()
+      } else {
+        console.log("error creating issue")
+      }
 
     }
   }
 
   function issue() {
-    let uri = '/it/api/issues/' + window.location.search.substring(1).split("=")[1]
+    const issueId = window.location.search.substring(1).split("=")[1]
+    let uri = uriManager.getIssueUri(id)
     console.log(uri)
     fetch(uri)
       .then(rsp => rsp.json())

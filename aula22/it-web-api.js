6e123f3f5aeec21668055492678992cafa0cae10
Luís Falcão <lfalcao@cc.isel.pt>
2020-06-01 21:13:09 +0100

    Lesson 22:  Copy from lesson 21.

---
+++ aula22/it-web-api.js
@@ -0,0 +1,76 @@
+
+const url = require('url')
+
+const error = require('./error')
+
+const router = require('express').Router() 
+
+
+module.exports = function (itServices) {
+  router.get('/issues', getIssues)
+  router.get('/issues/:id', getIssue)
+  router.post('/issues', addIssue)
+  router.delete('/issues/:id', deleteIssue)
+  
+
+  Promise.prototype.sendResponse = sendResponse
+
+  return router;
+
+
+  // GET /it/api/issues
+  function getIssues(req, rsp) {
+    itServices.getIssues().sendResponse(rsp)
+  }
+
+
+  // GET /it/api/issues/:id
+  function getIssue(req, rsp) {
+    console.log(req.params.id)
+    itServices.getIssue(req.params.id).sendResponse(rsp)
+  }
+
+
+
+  // POST /it/api/issues
+  function addIssue(req, rsp) {
+
+    console.log("body received: ", req.body)
+    itServices.addIssue(req.body).sendResponse(rsp, 201)
+  }
+
+
+  // DELETE /it/api/issues/:id
+
+  function deleteIssue(req, rsp) {
+    itServices.deleteIssue(req.params.id).sendResponse(rsp)
+  }
+
+  ////////////// Internal auxiliary functions
+
+
+  function sendResponse(rsp, successStatusCode = 200, errStatusCode = 500) {
+    this.then(processSuccess(rsp, successStatusCode)).catch(processError(rsp, errStatusCode))
+  }
+
+  function processSuccess(rsp, statusCode) {
+    return processResponse(rsp, () => statusCode)
+  }
+
+  function processError(rsp, statusCode) {
+    return processResponse(rsp, (data) => error.toHttpStatusCode(data))
+  }
+
+  function processResponse(rsp, statusCodeSup) {
+    return function (data) {
+      rsp.statusCode = statusCodeSup(data)
+      rsp.setHeader("Content-Type", "application/json")
+      rsp.end(JSON.stringify(data))
+    }
+  }
+}
+
+
+
+
+
